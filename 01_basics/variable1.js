86e3657e844b72b3db458d2187da7b1d5cf8edad
Shashank Singh <singhshashank7171@gmail.com>
2025-02-23 15:57:14 +0530

    variables

---
+++ 01_basics/variable1.js
@@ -0,0 +1,12 @@
+const accountId=14453;
+let accountEmail ="shashanksingh4330@gmail.com"
+var accountpassword="12345"
+accountcity ="jaipur"
+
+
+accountEmail="singhshashank7171@gmail.com"
+accountpassword="7777777"
+accountcity="delhi"
+
+console.log(accountEmail);
+console.log([accountEmail,accountId,accountpassword,accountcity])
