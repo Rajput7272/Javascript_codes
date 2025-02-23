f6539d145005af8b96c2d015f249c7da92387f56
Shashank Singh <singhshashank7171@gmail.com>
2025-02-23 16:37:28 +0530

    datatype

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,22 @@
+//alert(3+3) 
+console.log(3+3)
+console.log("Shashank")
+let name="shashank"
+let age=21
+let isdigit= false
+let state;
+
+// number => 2 to power 53
+// bigInt
+//string => ""
+//boolean => true/false
+//null =>standalonevalue
+//undefined =>
+//symbol=> unique
+
+//object
+
+console.log(typeof age);
+console.log(typeof undefined); //undefined
+console.log(typeof null);  // object
+
