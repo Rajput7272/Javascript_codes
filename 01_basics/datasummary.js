6d7b18de2ec00ca5e152818f8621e1e6cc4baa52
Shashank Singh <singhshashank7171@gmail.com>
2025-02-24 17:26:45 +0530

    summary

---
+++ 01_basics/datasummary.js
@@ -0,0 +1,29 @@
+// Peimitive 
+
+// 7 Types
+// String , Number , Boolean, Null , Undefined, Symbol,BigINT
+ const score= 100;
+ const scorevalue=100.3;
+
+ const isloggedIn=false;
+ const temperature=null;
+ let userEmail;
+
+ const id=Symbol('123')
+ const anotherid=symbol('123')
+
+
+// Reference Type(Non - Primitive)
+
+// Arrays , Objects , Functions
+
+const heros =["batman", " superman"];
+let myobj = {
+    name:  "hitesh",
+    age : 22,
+}
+
+const myFunction=function(){
+    console.log("Hello World");
+}
+console.log(typeof BigInt);
